7d3248200442a2be2aa55d636299b3d78d05ad81
Sonali Gupta <36319762+slytherin20@users.noreply.github.com>
2020-10-21 17:49:32 +0530

    Delete http_module.js

--- http_module.js
+++
@@ -1,8 +0,0 @@
-const http=require('http');
-const server=http.createServer();
-
-server.on('connection',(socket)=>{
-   console.log('New connection');
-});
-server.listen(3000);
-console.log("Listening on port 3000..");
